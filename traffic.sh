1a2532fa9bff9811a17c8f1ef1a112afd5a01972
Jordan Sissel <jls@semicomplete.com>
2007-11-24 10:18:11 +0000

    git-svn-id: https://semicomplete.googlecode.com/svn/traffic@1617 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- traffic.sh
+++ traffic.sh
@@ -2,13 +2,13 @@
 
 title="traffic $$"
 xulrunner application.ini --url "$1" --title "$title" &
-sleep 5
+sleep 8
 
 wid="$(xdotool search --title "$title")"
 tmp="$(mktemp /tmp/traffic.jpg.XXXXXXX)"
 rm $tmp
 tmp="$tmp.jpg"
-import -window $wid $tmp
+import -window $wid -rotate 90 $tmp
 echo $tmp
 
 pkill -f "xulrunner.*$title"
